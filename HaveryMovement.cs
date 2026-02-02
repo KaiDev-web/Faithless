27ebbdb982ca6d83b670de275c8da5c4585b6209
KaiDev-web <devacc983@gmail.com>
2026-02-02 15:40:55 +0100

    HaveryMovement.cs creato.

---
+++ HaveryMovement.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class HaveryMovementScript : MonoBehaviour
+{
+    public float walkSpeed = 6f;
+    public float runSpeed = 10f;
+    public float currentSpeed;
+
+    private Rigidbody2D rb;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.constraints = RigidbodyConstraints2D.FreezeRotation;
+    }
+
+    void FixedUpdate()
+    {
+        if(Keyboard.current.leftShiftKey.isPressed || Keyboard.current.rightShiftKey.isPressed)
+        {
+            currentSpeed = runSpeed;
+        }
+        else
+        {
+            currentSpeed = walkSpeed;
+        }
+
+        if(Keyboard.current.leftArrowKey.isPressed)
+        {
+            transform.Translate(Vector2.left * currentSpeed * Time.deltaTime);
+        }
+        
+        if(Keyboard.current.rightArrowKey.isPressed)
+        {
+            transform.Translate(Vector2.right * currentSpeed * Time.deltaTime);
+        }
+    }
+}
