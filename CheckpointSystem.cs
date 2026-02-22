5d26f1152268657a59441cf05d4d7a4c52eb0b35
KaiDev-web <devacc983@gmail.com>
2026-02-22 18:36:07 +0100

    CheckpointSystem.cs creato.

---
+++ CheckpointSystem.cs
@@ -0,0 +1,30 @@
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.Timeline;
+
+public class Checkpoint_System : MonoBehaviour
+{
+    public string keyX = "SavepointX";
+    public string keyY = "SavepointY";
+
+    void Start()
+    {
+        
+        float x = PlayerPrefs.GetFloat(keyX, transform.position.x);
+        float y = PlayerPrefs.GetFloat(keyY, transform.position.y);
+        transform.position = new Vector2(x, y);
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Checkpoint"))
+        {
+            PlayerPrefs.SetFloat(keyX, collision.transform.position.x);
+            PlayerPrefs.SetFloat(keyY, collision.transform.position.y);
+
+            PlayerPrefs.Save();
+
+            Debug.Log("Posizione salvata:" + collision.transform.position);
+        }      
+    }
+}
