f584fd8aa39284656a8f54982d155b95a2f4c7e6
KaiDev-web <devacc983@gmail.com>
2026-02-26 21:13:22 +0100

    SceneTransition.cs creato.

---
+++ SceneTransition.cs
@@ -0,0 +1,34 @@
+using Unity.VectorGraphics;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.SceneManagement;
+
+public class Scene_Transition : MonoBehaviour
+{
+    public string nomeScenaDaCaricare;
+
+    void Start()
+    {
+        if(Transition.deveTeletrasportarsi)
+        {
+            transform.position = Transition.posizioneDiArrivo;
+            Transition.deveTeletrasportarsi = false;
+        }
+    }
+
+    void OnEnterTrigger2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player Spritesheet_0"))
+        {
+            SceneManager.LoadScene(nomeScenaDaCaricare);
+        }  
+    }
+    public void TransitionStart(Vector2 nuovaPosizione, string nomeScena)
+    {
+        Transition.posizioneDiArrivo = nuovaPosizione;
+        Transition.deveTeletrasportarsi = true;
+
+        UnityEngine.SceneManagement.SceneManager.LoadScene(nomeScena);
+    }
+    
+}
