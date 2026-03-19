6eaaf5242ccbe6a3a1a3ee065a11b78ede1f6c48
KaiDev-web <devacc983@gmail.com>
2026-03-19 19:23:09 +0100

    AttackSystem.cs aggiornato.

--- AttackSystem.cs
+++ AttackSystem.cs
@@ -6,28 +6,59 @@ using UnityEngine.InputSystem;
 public class AttackSystem : MonoBehaviour
 {
     public GameObject attackHitBox;
+    public GameObject attackHitBoxDown;
+    public GameObject attackHitBoxUp;
     public float hitboxWaitTime = 1;
 
     public void Start()
     {
         attackHitBox.gameObject.SetActive(false);
+        attackHitBoxDown.gameObject.SetActive(false);
+        attackHitBoxUp.gameObject.SetActive(false);
     }
     
     private void Update()
     {
         if (Keyboard.current.xKey.wasPressedThisFrame)
         {
-            Debug.Log("Dude this attack is fire.");
+            Debug.Log("Right/Left.");
 
-            StartCoroutine(AttackExecute());
+            StartCoroutine(AttackExecuteLeft());
+        }
+
+        if (Keyboard.current.xKey.wasPressedThisFrame && Keyboard.current.downArrowKey.isPressed)
+        {
+            Debug.Log("Down.");
+
+            StartCoroutine(AttackExecuteDown());
+        }
+
+        if (Keyboard.current.xKey.wasPressedThisFrame && Keyboard.current.upArrowKey.isPressed)
+        {
+            Debug.Log("Up.");
+
+            StartCoroutine(AttackExecuteUp());
         }
     }
 
-    IEnumerator AttackExecute()
+    IEnumerator AttackExecuteLeft()
     {
         attackHitBox.gameObject.SetActive(true);
         yield return new WaitForSeconds(hitboxWaitTime);
         attackHitBox.gameObject.SetActive(false);
-        
+    }
+
+    IEnumerator AttackExecuteDown()
+    {
+        attackHitBoxDown.gameObject.SetActive(true);
+        yield return new WaitForSeconds(hitboxWaitTime);
+        attackHitBoxDown.gameObject.SetActive(false);
+    }
+
+    IEnumerator AttackExecuteUp()
+    {
+        attackHitBoxUp.gameObject.SetActive(true);
+        yield return new WaitForSeconds(hitboxWaitTime);
+        attackHitBoxUp.gameObject.SetActive(false);
     }
 }
